1027e7d24060514dddfdb6872ecb1c22f982a41e
TheOrangeInd <TheOrangeInd@DESKTOP-AAO5NUL>
2019-03-10 19:56:22 +0200

    lab1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//task 8
+
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            double divident, divider, result;
+
+            Console.WriteLine("Нахождение частного чисел\nВведите сначала делимое, а затем делитель(не равный нулю)");
+            try
+            {
+                divident = Convert.ToDouble(Console.ReadLine());
+            } catch
+            {
+                Console.WriteLine("Неверный формат ввода (делимое)");
+                Console.ReadLine();
+                return;
+            }
+
+            try
+            {
+                divider = Convert.ToDouble(Console.ReadLine());
+            } catch
+            {
+                Console.WriteLine("Неверный формат ввода (делитель)");
+                Console.ReadLine();
+                return;
+            }
+
+            if (divider == 0) Console.WriteLine("Нельзя делить на 0");
+            else
+            {
+                result = divident / divider;
+                Console.WriteLine(divident + " / " + divider + " = " + result);
+            }
+
+            Console.ReadLine();
         }
     }
 }
